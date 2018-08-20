1e9bd9bb046afd252492ecc34473edea2037622d
Sydney Moutia <sydney@akhann.com>
2018-08-20 11:08:28 +0400

    Basic Class declaration

--- index.js
+++ index.js
@@ -9,6 +9,36 @@ class Product {
     }
 }
 
-const product = new Product('Pencil', 14);
-product.show();
+class Order {
+    construct() {
+        this.items = [];
+    }
+
+    add(product) {
+        item.push(product);
+    }
+
+    showTotal() {
+        console.log('Order Total', this.calculateTotal());
+    }
+
+    calculateTotal() {
+        var sum = 0;
+
+        for (var i = 0, len = this.items; i < len; i++) {
+            sum += this.items[i].price;
+        }
+
+        return sum;
+    }
+}
+
+const product1 = new Product('Pencil', 14);
+const product2 = new Product('Casing', 18);
+
+const order = new Order();
+order.add(product1);
+order.add(product2);
+
+showTotal();
 
